5ead24e6bce903ba1e154cc671a6755772356fca
Diablo-bit <anshgoel01@gmail.com>
2021-11-07 04:12:31 +0530

    files added

---
+++ app/src/main/java/com/example/imagepicker20/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.imagepicker20;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.provider.MediaStore;
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    ImageView myImage;
+    Button button;
+    ActivityResultLauncher<Intent> activityResultLauncher;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myImage = (ImageView) findViewById(R.id.myImage);
+        button = (Button) findViewById(R.id.button);
+
+        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if(result.getResultCode() == RESULT_OK && result.getData()!= null){
+                    Bundle bundle = result.getData().getExtras();
+                    Bitmap bitmap = (Bitmap) bundle.get("data");
+                    myImage.setImageBitmap(bitmap);
+            }
+        }
+                });
+
+       button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                if (intent.resolveActivity(getPackageManager())!= null)
+                {
+                    activityResultLauncher.launch(intent);
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this, "No app supports this action",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+      }
+
+    }
